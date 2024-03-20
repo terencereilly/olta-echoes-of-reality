52c4d00efb692ea96b708a60c10df5853a73ed23
Eoghan O'Keeffe | epok.tech <epok.tech@gmail.com>
2024-03-20 16:47:45 -0400

    Refactor for multiple team meshes, fixes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
+/**
+ * @todo Use collision detection and spatial partition to constrain forms from
+ *   interpenetration while altering radius... but it's tricky, for now just
+ *   allow interpenetration and try balance things with the growth/shrink rates.
+ */
+
 import { Olta } from '@olta/js-sdk';
 
 import {
     WebGLRenderer, Scene, Raycaster, PerspectiveCamera, Clock,
     Vector2, Vector3, Vector4, Quaternion, Matrix4, Spherical, Box3, Sphere,
-    InstancedMesh, IcosahedronGeometry, MeshStandardMaterial,
-    HemisphereLight, PointLight, Color, MathUtils, Fog, DoubleSide
+    InstancedMesh, SphereGeometry, CylinderGeometry, MeshStandardMaterial,
+    HemisphereLight, PointLight, Color, MathUtils, Fog, DoubleSide,
+    BufferGeometry, Mesh
   } from 'three';
 
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 import CameraOrbitControls from 'camera-controls';
 
-import { each } from '@epok.tech/fn-lists/each';
+import { computeBoundsTree, disposeBoundsTree, acceleratedRaycast }
+  from 'three-mesh-bvh';
 
-const { random, round } = Math;
-const { MAX_SAFE_INTEGER: intMax } = Number;
+import { each } from '@epok.tech/fn-lists/each';
+import { map } from '@epok.tech/fn-lists/map';
+import { range } from '@epok.tech/fn-lists/range';
+
+const { random, round, floor, max, abs, acos, PI: pi } = Math;
+const { MAX_SAFE_INTEGER: intMax, EPSILON: eps } = Number;
 const { lerp } = MathUtils;
 
 const api = self.echoes = {};
+
+let wallet;
 
 const depths = api.depths = [1, 1e4];
 const [near, far] = depths;
@@ -29,6 +43,11 @@ const intScale = api.intScale = 1e-6;
 const radii = api.radii = [1, 1e2];
 const [r0, r1] = radii;
 
+const teams = api.teams = 3;
+const team = api.team = floor(random()*teams);
+const chase = (t) => (t+1)%teams;
+const flees = (t) => (t+2)%teams;
+
 const colors = api.colors = {
   any: [0x00bbbb, 0xbb00bb, 0xbbbb00],
   own: [0x44ffff, 0xff44ff, 0xffff44]
@@ -38,6 +57,11 @@ const $body = api.$body = document.body;
 const $canvas = api.$canvas = document.querySelector('canvas');
 
 const size = new Vector2();
+
+// @todo Use a simpler sphere intersection test instead.
+BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
+BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
+Mesh.prototype.raycast = acceleratedRaycast;
 
 CameraOrbitControls.install({
   THREE: {
@@ -50,43 +74,85 @@ const renderer = api.renderer =
   new WebGLRenderer({ canvas: $canvas, antialias: true });
 
 renderer.setPixelRatio(devicePixelRatio);
+renderer.setClearColor(0xffffff);
 
 const scene = api.scene = new Scene();
 const clock = api.clock = new Clock();
+
 const camera = api.camera = new PerspectiveCamera(60, 1, ...depths);
+
+// camera.position.set(0, 0, eps);
+camera.position.set(0, 0, 1e-5);
+
 const orbit = api.orbit = new CameraOrbitControls(camera, $canvas);
 
 orbit.infinityDolly = orbit.dollyToCursor = true;
-orbit.dollyTo(orbit.minDistance = orbit.maxDistance = lerp(...depths, 0.2));
+// orbit.infinityDolly = true;
+orbit.minDistance = orbit.maxDistance = r1;
+orbit.setPosition(0, 0, 1e3, false);
+orbit.saveState();
 
-scene.fog = new Fog(0x000000, ...depths);
+scene.fog = new Fog(0xffffff, ...depths);
 
-const lightCamera = api.lightCamera = new PointLight(0xffffff);
+const lightCamera = api.lightCamera = new PointLight(0xffffff, 1e5);
 
-lightCamera.position.set(0, r1*2, 0);
+lightCamera.position.set(0, r1, -r1);
 scene.add(camera.add(lightCamera));
 
-const lightSky = api.lightSky = new HemisphereLight(0xffffff, 0x000000, 3);
+const lightSky = api.lightSky = new HemisphereLight(0xffffff, 0x000000, 0.5);
 
 scene.add(lightSky);
 
-const geometry = api.geometry = new IcosahedronGeometry(1, 3);
-const material = api.material = new MeshStandardMaterial();
+const forms = api.forms = {
+  geometry: new SphereGeometry(1),
+  material: new MeshStandardMaterial(),
+  transform: new Matrix4(),
+  color: new Color(),
+  data: [],
+  meshes: [],
+  limits: []
+};
 
-const transform = new Matrix4();
-const color = new Color();
-let forms;
-let fl;
+forms.geometry.computeBoundsTree();
+
+const bvh = forms.geometry.boundsTree;
+const sphere = new Sphere(undefined, 1);
+
+const hint = api.hint = new Mesh(forms.geometry, new MeshStandardMaterial({
+  transparent: true, opacity: 0.3, side: DoubleSide, depthWrite: false
+}));
+
+hint.visible = false;
+hint.material.color.setHex(colors.own[team]);
+scene.add(hint);
+
+const hit = api.hit = new Mesh(new CylinderGeometry(0.1, 1, 2, 2**5, 1, true),
+  new MeshStandardMaterial({
+    transparent: true, opacity: 0.5, side: DoubleSide, depthWrite: false
+  }));
+
+hit.visible = false;
+hit.material.color.setHex(0xffffff);
+hit.material.emissive.setHex(0xcccccc);
+hit.geometry.rotateX(pi*0.5);
+scene.add(hit);
 
 const raycaster = api.raycaster = new Raycaster();
-const pointer = api.pointer = new Vector2(1, 1);
+
+raycaster.firstHitOnly = true;
+
+const pointer = api.pointer = new Vector2();
 const axes = api.axes = new Vector2(1, -1);
 
 stats = api.stats = new Stats();
 $body.appendChild(stats.dom);
 
-const render = api.render = () =>
+let needRender = false;
+
+const render = api.render = () => {
   console.log('render', renderer.render(scene, camera));
+  needRender = false;
+};
 
 const resize = api.resize = () => {
   const { width: w, height: h } = $canvas.getBoundingClientRect();
@@ -103,15 +169,64 @@ const frame = api.frame = () => {
 
   const dt = clock.getDelta();
 
-  orbit.update(dt) && render();
+  (orbit.update(dt) || needRender) && render();
   stats.update();
 }
 
 $canvas.addEventListener('pointermove', ({ clientX: x, clientY: y }) => {
+  const { meshes, data } = forms;
+  const to = meshes[team];
+
+  needRender = true;
+
+  if(!to) { return hit.visible = hint.visible = false; }
+
   pointer.set(x, y).divide(size).multiplyScalar(2).subScalar(1).multiply(axes);
   raycaster.setFromCamera(pointer, camera);
-  console.log('???', forms && raycaster.intersectObject(forms));
+
+  const [at] = raycaster.intersectObjects(meshes);
+
+  if(!at) { return hit.visible = hint.visible = false; }
+
+  const { object: mesh, instanceId: i, point: p, normal: n } = at;
+  const t = meshes.indexOf(mesh);
+  const form = data[t][i];
+  const { r } = form;
+
+  console.log('hit', at, p, mesh, form);
+  hit.visible = true;
+  hit.position.copy(p);
+  hit.scale.setScalar(r*0.2);
+  hit.lookAt(p.add(n));
+
+  if(t !== team) { return hint.visible = false; }
+
+  console.log('hint');
+  hint.visible = true;
+  hint.position.copy(p);
+  // Starting radius.
+  hint.scale.setScalar(r);
+
+  // Check the other team forms with `intersectSphere`, increase radius if
+  // touching a team to chase, decrease if touching a team this flees (always).
+  const { [chase(team)]: mc, [flees(team)]: mf } = meshes;
+
+  // sphere.applyMatrix4();
+
+  // const  = bvh.intersectsSphere(sphere);
 });
+
+// @todo Find a way to sensibly set the camera to orbit around the intersection.
+// orbit.addEventListener('controlstart', () => {
+//   const { visible: v, position: p } = hit;
+
+//   if(!v) { return; }
+
+//   const { x: px, y: py, z: pz } = p;
+
+//   orbit.setOrbitPoint(px, py, pz);
+//   needRender = true;
+// });
 
 addEventListener('resize', resize);
 resize();
@@ -119,141 +234,75 @@ frame();
 
 const olta = api.olta = Olta();
 
-const intToData = api.intToData = (int) => (int | 0)+'n';
-const dataToInt = api.dataToInt = (data) => parseInt(data, 10);
-
-const formToData = api.formToData = (form, to = {}) => {
-  const { t, r, x, y, z } = form;
-
-  to || (to = form);
-  to.t = intToData(t);
-  to.r = intToData(r);
-  to.x = intToData(x);
-  to.y = intToData(y);
-  to.z = intToData(z);
-
-  return to;
-};
-
-const dataToForm = api.dataToForm = (data, to = {}) => {
-  const { t, r, x, y, z } = data;
-
-  to || (to = data);
-  to.t = dataToInt(t);
-  to.r = dataToInt(r);
-  to.x = dataToInt(x);
-  to.y = dataToInt(y);
-  to.z = dataToInt(z);
-
-  return to;
-};
-
-const clearForms = api.clearForms = () =>
-  fl = forms = forms?.removeFromParent?.().dispose?.();
-
-const dataToScene = api.dataToScene = (data) => {
+const dataToScene = api.dataToScene = (to) => {
   // @todo Construct a KD-Tree, and only create enough meshes for nearby range.
-  const dl = data.length;
+  const tl = to.length;
+  const { geometry, material, transform, color, data, meshes, limits } = forms;
 
-  if(!dl) { return clearForms(); }
-  else if((fl ?? 0) < dl) {
-    clearForms();
-    scene.add(forms = new InstancedMesh(geometry, material, fl = dl));
+  if(!tl) {
+    each((m) => m.removeFromParent().dispose(), meshes);
+
+    return data.length = limits.length = meshes.length = 0;
   }
-  else { forms.count = dl; }
 
-  each((d, i) => {
-      const { t, r, x, y, z, _creator } = dataToForm(d, 0);
+  // Group data by team.
+  each((v) => (data[v.t] ??= []).push(v), to);
+
+  each((d, t) => {
+      const dl = d.length;
+      let m = meshes[t];
+
+      // Set up a larger instanced mesh if needed.
+      if(!m || ((limits[t] ?? 0) < dl)) {
+        m?.removeFromParent?.().dispose?.();
+
+        scene.add(m = meshes[t] =
+          new InstancedMesh(geometry, material, limits[t] = dl));
+      }
+      // Just reduce the instance draw count if the limit is high enough.
+      else { m.count = dl; }
+
+      // Set up the mesh instances.
       const s = intScale;
 
-      forms.setMatrixAt(i,
-        transform.makeScale(r, r, r).setPosition(x*s, y*s, z*s));
+      each(({ r, x, y, z, _creator }, i) => {
+          m.setMatrixAt(i,
+            transform.makeScale(r, r, r).setPosition(x*s, y*s, z*s));
 
-      // @todo Alternate color for forms added by the same creator.
-      forms.setColorAt(i,
-        color.setHex(colors[(_creator === 'wallet')? 'own' : 'any'][t]));
+          // @todo Alternate color for forms added by the same creator.
+          m.setColorAt(i,
+            color.setHex(colors[(_creator === wallet)? 'own' : 'any'][t]));
+        },
+        d);
+
+      m.instanceMatrix.needsUpdate = m.instanceColor.needsUpdate = true;
     },
     data);
 
-  forms.instanceMatrix.needsUpdate = forms.instanceColor.needsUpdate = true;
-  render();
+  needRender = true;
+  console.log('forms', forms);
 };
 
 const update = api.update = (state) => {
-  /**
-   * @todo Check `state` settings to use dynamically?
-   * @example ```
-   * {
-   *   collections: { forms: { ... } },
-   *   configs: {
-   *     forms: {
-   *       name: 'forms',
-   *       type: 'object',
-   *       properties: {
-   *         r: {
-   *           max: '100',
-   *           min: '1',
-   *           name: 'r',
-   *           type: 'bigint',
-   *           update: 'set'
-   *         },
-   *         t: {
-   *           max: '2',
-   *           min: '0',
-   *           name: 't',
-   *           type: 'bigint',
-   *           update: 'set'
-   *         },
-   *         x: {
-   *           max: '9007199254740991',
-   *           min: '-9007199254740991',
-   *           name: 'x',
-   *           type: 'bigint',
-   *           update: 'set'
-   *         },
-   *         y: {
-   *           max: '9007199254740991',
-   *           min: '-9007199254740991',
-   *           name: 'y',
-   *           type: 'bigint',
-   *           update: 'set'
-   *         },
-   *         z: {
-   *           max: '9007199254740991',
-   *           min: '-9007199254740991',
-   *           name: 'z',
-   *           type: 'bigint',
-   *           update: 'set'
-   *         }
-   *       },
-   *       permissions: {
-   *         create: 'open',
-   *         update: 'admin',
-   *         createType: 'set',
-   *         updateType: 'set'
-   *       }
-   *     }
-   *   }
-   * }
-   * ```
-   */
-
   const to = olta.getAll('forms');
 
   console.log('data', to, state);
-  to && dataToScene(to);
-  console.log('forms', forms);
+
+  if(!to) { return; }
+
+  dataToScene(to);
+  wallet = state.activeWalletAddress;
 };
 
 olta.onUpdate(update);
 
 // document.body.addEventListener('click', () =>
 //   // @todo Check `state` settings to use dynamically, for limits etc?
-//   olta.create('forms', formToData({
+//   olta.create('forms', {
 //       t: round(random()*colors.any.length),
 //       r: round(lerp(...radii, random())),
 //       x: round(lerp(...bounds, random())),
 //       y: round(lerp(...bounds, random())),
 //       z: round(lerp(...bounds, random()))
 //     },
-//     0)));
+//     0));
