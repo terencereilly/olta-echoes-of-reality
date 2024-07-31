86afdd258a36cd5aa02ae3f714bd3874381d71c6
Eoghan O'Keeffe | epok.tech <epok.tech@gmail.com>
2024-07-31 11:38:02 +0100

    Main animations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 /**
+ * @todo Intro and team selection interaction.
+ * @todo Icons to explain `hunt`, `team`, `flee` interaction.
+ * @todo Setup an admin mode to allow update/delete to moderate contributions.
  * @todo Address Terrence's Figma notes.
- * @todo Animations: hint radius.
- * @todo Replay time: add shapes in sequence (by `_sortKey` or `_id`?),
- *   on intro or idle.
  * @todo Offset touch target to above fingertip so it's visible.
  * @todo Distinct touch interactions for moving target versus camera.
- * @todo Make camera move more consistently, especially dolly out...
- * @todo Icons to explain `hunt`, `team`, `flee` interaction.
  * @todo Style updates according to Terrence's mood, colour, post references.
+ * @todo Make camera move more consistently, especially dolly out...
+ * @todo Replay time: add shapes in sequence (by `_sortKey` or `_id`?),
+ *   on intro or idle.
  * @todo Maybe add sphere BVH to optimise later.
  * @todo Use collision detection and spatial partition to constrain forms from
  *   interpenetration while altering radius... but it's tricky, for now just
@@ -23,7 +24,9 @@ import {
     HemisphereLight, PointLight, Color, MathUtils, Fog, DoubleSide,
     BufferGeometry, Mesh, Group, TextureLoader, NearestFilter,
     NumberKeyframeTrack, VectorKeyframeTrack,
-    AnimationClip, AnimationMixer, InterpolateSmooth, LoopOnce
+    AnimationClip, AnimationMixer,
+    InterpolateSmooth, InterpolateLinear,
+    LoopOnce
   } from 'three';
 
 import Stats from 'three/examples/jsm/libs/stats.module.js';
@@ -41,9 +44,16 @@ const gradientMapURL = new URL('assets/tone-5.jpg', import.meta.url);
 
 const { random, round, floor, min, max, abs, acos, PI: pi } = Math;
 const { MAX_SAFE_INTEGER: intMax, EPSILON: eps } = Number;
-const { lerp, clamp } = MathUtils;
+const { lerp, inverseLerp: unlerp, clamp } = MathUtils;
 
 const api = self.echoes = {};
+
+const cache = api.cache = {
+  transform: new Matrix4(),
+  colors: [new Color(), new Color()],
+  spheres: [new Sphere()],
+  vector3s: [new Vector3(), new Vector3(), new Vector3()]
+};
 
 const query = api.query = new URLSearchParams(location.search);
 
@@ -62,21 +72,32 @@ const [r0, r1] = radii;
 
 const boundRadius = (intMax*positionScale)+r1;
 
-const growth = 1.5;
-const shrink = 0.9;
-const inner = 0.95;
+const scales = api.scales = { growth: 1.5, shrink: 0.9, inner: 0.95, hit: 0.2 };
 
-const teams = api.teams = 3;
+const colors = api.colors = {
+  // any: [0x3033d4, 0xb02940, 0xefa53b],
+  teams: [
+    new Color('hsl(229, 82%, 46%)'),
+    new Color('hsl(309, 72%, 38%)'),
+    new Color('hsl(39, 95%, 48%)')
+  ],
+  // HSL offsets.
+  own: [0, 0.2, 0],
+  hunt: [0.05, 0, 0],
+  flee: [-0.05, 0, 0],
+  grow: [0, 0, 0.15]
+};
+
+const colorAt = (team, to = new Color()) => to.copy(colors.teams[team]);
+const colorTo = (by, to = new Color()) => to.offsetHSL(...colors[by]);
+
+const colorMix = (alpha, by, to = new Color(), c = new Color()) =>
+  to.lerpHSL(colorTo(by, c.copy(to)), alpha);
+
+const teams = api.teams = colors.teams.length;
 const team = api.team = parseInt(query.get('team') || floor(random()*teams));
 const hunt = (t) => (t+1)%teams;
 const flee = (t) => (t+2)%teams;
-
-const colors = api.colors = {
-  // any: [0x00bbbb, 0xbb00bb, 0xbbbb00],
-  // own: [0x44ffff, 0xff44ff, 0xffff44]
-  any: [0x3033d4, 0xb02940, 0xefa53b],
-  own: [0x44ffff, 0xff44ff, 0xffff44]
-};
 
 const $body = api.$body = document.body;
 const $canvas = api.$canvas = document.querySelector('canvas');
@@ -108,16 +129,18 @@ const camera = api.camera = new PerspectiveCamera(60, 1, ...depths);
 const orbit = api.orbit = new CameraOrbitControls(camera, $canvas);
 const orbitStart = boundRadius*2;
 
+orbit.maxSpeed = 1e3;
 orbit.infinityDolly = orbit.dollyToCursor = true;
 orbit.minDistance = orbit.maxDistance = r1;
-orbit.dollySpeed = 3;
+orbit.dollySpeed = 10;
 orbit.truckSpeed = 10;
 orbit.setLookAt(0, 0, orbitStart+orbit.minDistance, 0, 0, orbitStart, true);
 orbit.saveState();
 
-scene.fog = new Fog(0xffffff, ...depths);
+// scene.fog = new Fog(0xffffff, ...depths);
+scene.fog = new Fog(0x000000, ...depths);
 
-const lightCamera = api.lightCamera = new PointLight(0xffffff, 1e5);
+const lightCamera = api.lightCamera = new PointLight(0xffffff, 5e4, 0, 1.7);
 
 lightCamera.position.set(0, r1, 0);
 scene.add(camera.add(lightCamera));
@@ -139,9 +162,6 @@ const forms = api.forms = {
 
 forms.geometry.computeBoundsTree();
 
-const spheres = [new Sphere()];
-const vector3s = [new Vector3(), new Vector3(), new Vector3()];
-
 function dataToSphere({ x, y, z, r }, s) {
   s.center.set(x, y, z).multiplyScalar(positionScale);
   s.radius = r;
@@ -149,29 +169,36 @@ function dataToSphere({ x, y, z, r }, s) {
   return s;
 }
 
-const hint = api.hint = new Mesh(forms.geometry, new MeshToonMaterial({
-  transparent: true, opacity: 0.3, side: DoubleSide, depthWrite: false,
-  color: colors.own[team], gradientMap
-}));
+const hint = api.hint = {
+  mesh: new Mesh(forms.geometry, new MeshToonMaterial({
+    transparent: true, opacity: 0.4, side: DoubleSide, depthWrite: false,
+    color: colorTo('own', colorAt(team)), gradientMap
+  })),
+  radius: 1, ease: 1e1,
+  tween(dt) {
+    const { radius: r, ease: e, mesh: { scale: s } } = hint;
+    const sr = s.x;
 
-hint.visible = false;
-
-const hold = api.hold = {
-  mesh: new Mesh(hint.geometry, hint.material.clone()),
-  frames: {}, wait: 1e3
+    s.setScalar(sr+((r-sr)*e*dt));
+  }
 };
 
-scene.add(hint.add(hold.mesh));
+const hold = api.hold = {
+  mesh: new Mesh(hint.mesh.geometry, hint.mesh.material.clone()),
+  frames: {}, wait: 1e3,
+  at: new Vector2(), moved2: 20
+};
 
-(() => {
+scene.add(hint.mesh.add(hold.mesh));
+
+(function animateHold() {
   const { mesh, frames, wait } = hold;
   const mixer = hold.mixer = new AnimationMixer(mesh);
   const { material, scale } = mesh;
-  const o1 = material.opacity;
   const times = [0, wait*1e-3];
 
   const fo = frames.opacity = new NumberKeyframeTrack('.material.opacity',
-    times, [1, o1], InterpolateSmooth);
+    times, [1, material.opacity*0.1], InterpolateSmooth);
 
   const fs = frames.scale = new VectorKeyframeTrack('.scale',
     times, scale.toArray(range(6, eps), 3), InterpolateSmooth);
@@ -184,8 +211,8 @@ scene.add(hint.add(hold.mesh));
 
 const hit = api.hit = new Mesh(new CylinderGeometry(0.1, 0.5, 1, 2**5, 1, true),
   new MeshToonMaterial({
-    transparent: true, opacity: 0.5, side: DoubleSide, depthWrite: false,
-    color: colors.own[team], fog: false, gradientMap
+    transparent: true, opacity: 0.6, side: DoubleSide, depthWrite: false,
+    color: hint.mesh.material.color.clone(), fog: false, gradientMap
   }));
 
 hit.visible = false;
@@ -198,14 +225,14 @@ const traces = api.traces = {
   mesh: new Mesh(forms.geometry,
     new MeshToonMaterial({
       transparent: true, opacity: 0.6, side: DoubleSide, depthWrite: false,
-      color: colors.own[team], fog: false, gradientMap
+      color: hint.mesh.material.color.clone(), fog: false, gradientMap
     })),
   frames: { drawRangeSpeed: 1 }
 };
 
 // @todo Work out how to animate the `drawRange` here... can't parse the path.
 // @todo Animate the clip planes of the `hint`.
-(() => {
+(function animateTrace() {
   const { mesh, frames } = traces;
   const mixer = traces.mixer = new AnimationMixer(mesh);
   const o1 = mesh.material.opacity;
@@ -222,10 +249,11 @@ const traces = api.traces = {
 function toTrace(x, y, z, r) {
   const { mesh, group } = traces;
   const t = mesh.clone();
-  const { position: p, scale: s } = t;
+  const { position: p, scale: s, material } = t;
 
   p.set(x, y, z);
-  s.multiplyScalar(r*inner);
+  s.multiplyScalar(r*scales.inner);
+  material.color.copy(hint.mesh.material.color);
   // @todo Track the time it was added, to remove when its data arrives?
   group.add(t);
 
@@ -238,22 +266,21 @@ const raycaster = api.raycaster = new Raycaster();
 
 raycaster.firstHitOnly = true;
 
-const transform = new Matrix4();
-const color = new Color();
-const pointer = api.pointer = new Vector2();
-const axes = api.axes = new Vector2(1, -1);
+const pointer = new Vector2();
+const axes = new Vector2(1, -1);
 
 const stats = api.stats = ((query.has('stats'))? new Stats() : null);
 
 stats && $body.appendChild(stats.dom);
 
 // let needRender = false;
-let needRender = true;
+const needRender = true;
 
 const render = api.render = (dt = 0) => {
   // console.log('render');
   hold.mixer.update(dt);
   traces.mixer.update(dt);
+  hint.tween(dt);
   renderer.render(scene, camera);
 
   // const { geometry: g, frames: { drawRangeSpeed: drs } } = traces;
@@ -284,43 +311,48 @@ const frame = api.frame = () => {
 
   (orbit.update(dt) || needRender) && render(dt);
   stats?.update?.();
-}
+};
 
 const clearHeld = api.clearHeld = () => {
   const { held, action, mesh } = hold;
 
   action.stop();
   mesh.visible = false;
-
-  delete hold.held;
   clearTimeout(held);
+  delete hold.held;
 
   return held;
 };
 
-$canvas.addEventListener('pointermove', clearHeld);
 $canvas.addEventListener('pointerup', clearHeld);
 $canvas.addEventListener('pointercancel', clearHeld);
 $canvas.addEventListener('pointerout', clearHeld);
 $canvas.addEventListener('pointerleave', clearHeld);
 
+$canvas.addEventListener('pointermove', (e) => {
+  const { held, at, moved2 } = hold;
+
+  held && (at.distanceToSquared(e) > moved2) && clearHeld();
+});
+
 // @todo Animate hint color over time.
-$canvas.addEventListener('pointerdown', () => {
-  const { mesh, wait, action } = hold;
+$canvas.addEventListener('pointerdown', (e) => {
+  const { mesh, wait, action, at } = hold;
 
   clearHeld();
+  mesh.material.color.copy(hint.mesh.material.color);
   mesh.visible = true;
   action.play();
+  at.copy(e);
 
   hold.held = setTimeout(() => {
       if(!clearHeld()) { return; }
 
-      const { visible: hv, scale: hs, position: hp } = hint;
+      const { radius: r, mesh: { visible: hv, position: hp } } = hint;
 
-      if(!hv) { return; }
+      if(!r || !hv) { return; }
 
       const { x, y, z } = hp;
-      const r = hs.x;
       const ps = positionScale;
 
       // @todo Check `state` settings to use dynamically, for limits etc?
@@ -335,25 +367,25 @@ $canvas.addEventListener('pointerdown', () => {
       console.log('create', to);
       olta.create('forms', to);
       toTrace(x, y, z, r);
-      hint.visible = mesh.visible = false;
+      mesh.visible = false;
+      hint.radius = 0;
       // needRender = true;
     },
     wait);
 });
 
-$canvas.addEventListener('pointermove', ({ clientX: x, clientY: y }) => {
+$canvas.addEventListener('pointermove', (e) => {
   const { data } = forms;
 
   // needRender = true;
 
-  if(!data?.length) { return hit.visible = hint.visible = false; }
+  if(!data?.length) { return hint.radius = +(hit.visible = false); }
 
-  pointer.set(x, y).divide(size).multiplyScalar(2).subScalar(1).multiply(axes);
+  pointer.copy(e).divide(size).multiplyScalar(2).subScalar(1).multiply(axes);
   raycaster.setFromCamera(pointer, camera);
 
   const { ray, ray: { origin: ro } } = raycaster;
-  const [s] = spheres;
-  const [p, v, c] = vector3s;
+  const { spheres: [s], vector3s: [p, v, c], colors: [color] } = cache;
   let at;
 
   const l2 = reduce((l2, ds) =>
@@ -373,51 +405,60 @@ $canvas.addEventListener('pointermove', ({ clientX: x, clientY: y }) => {
       ds, l2),
     data, Infinity);
 
-  if(l2 === Infinity) { return hit.visible = hint.visible = false; }
+  if(l2 === Infinity) { return hint.radius = +(hit.visible = false); }
 
   const { t } = at;
-  const ar = at.r;
+  let ar = at.r;
+  const { growth: sg, shrink: ss, hit: sh } = scales;
+  const hitColor = hit.material.color;
 
   hit.lookAt(v.copy(hit.position.copy(p)).add(p).sub(c));
   hit.visible = true;
 
-  if(t === team) { hit.material.color.setHex(0x000000); }
-  else {
-    hit.material.color.setHex(colors.own[team]);
-    hit.scale.setScalar(ar*0.2);
+  if(t !== team) {
+    // colorTo(((t === hunt(team))? 'hunt' : 'flee'),
+    //   colorTo('own', colorAt(team, hitColor)));
+    colorTo('own', colorAt(team, hitColor));
+    hit.scale.setScalar(ar*sh);
 
-    return hint.visible = false;
+    return hint.radius = 0;
   }
 
   // Check the other team forms intersected:
   // - Increase radius if touching a team to `hunt`.
   // - Decrease radius to avoid touching a team to `flee`.
   const { [hunt(team)]: dh, [flee(team)]: df } = data;
-  let r = ar;
+  let r = ar = max(ar*ss, r0);
 
   // Check `hunt` first, increase radius if touched, proportional to how much of
   // each form (radius) is crossed.
   // @todo Optimise using squared-distance comparisons.
-  r *= lerp(1, growth,
-    clamp(reduce((to, d) =>
-          to+(clamp((r-dataToSphere(d, s).distanceToPoint(p))/r, 0, 1)),
-        dh, 0),
-      0, 1));
+  r *= min(sg,
+    reduce((to, d) =>
+        to+max(min(r-dataToSphere(d, s).distanceToPoint(p), s.radius, r)/r, 0),
+      dh, 1));
 
   // Check `flee` and decrease radius to nearest touch.
   // @todo Optimise using squared-distance comparisons.
   r = reduce((to, d) => min(to, dataToSphere(d, s).distanceToPoint(p)), df, r);
 
   // @todo Animate hint radius over time.
-  if(r < r0) { return hint.visible = false; }
+  if(r < r0) { return hint.radius = 0; }
 
-  hint.scale.setScalar(min(r*shrink, r1));
-  hint.position.copy(p);
+  const hintColor = colorAt(team, hint.mesh.material.color);
+
+  if(r === ar) { hitColor.setHex(0); }
+  else {
+    colorTo(((r < ar)? 'flee' : 'hunt'), hintColor);
+    colorAt(((r < ar)? flee : hunt)(team), hitColor);
+  }
+
+  r = min(r, r1);
+  colorMix(unlerp(...radii, r), 'grow', hintColor, color);
+  hint.radius = r;
+  hint.mesh.position.copy(p);
   hint.visible = true;
-  hit.scale.setScalar(r*0.2);
-
-  // @todo Interpolate colour to mix towards `hunt` (if `r > ar`) or
-  // `flee` (if `r < ar`), then animate back to its normal colour.
+  hit.scale.setScalar(r*sh);
 });
 
 // @todo Find a way to sensibly set the camera to orbit around the intersection.
@@ -442,6 +483,7 @@ const dataToScene = api.dataToScene = (to) => {
   // @todo Construct a KD-Tree, and only create enough meshes for nearby range.
   const tl = to.length;
   const { geometry, material, data, meshes, limits } = forms;
+  const { transform, colors: [c0, c1] } = cache;
 
   if(!tl) {
     each((m) => m.removeFromParent().dispose(), meshes);
@@ -476,9 +518,10 @@ const dataToScene = api.dataToScene = (to) => {
           m.setMatrixAt(i,
             transform.makeScale(r, r, r).setPosition(x*ps, y*ps, z*ps));
 
-          // @todo Alternate color for forms added by the same creator.
-          m.setColorAt(i,
-            color.setHex(colors[(_creator === wallet)? 'own' : 'any'][t]));
+          colorAt(t, c0);
+          (_creator === wallet) && colorTo('own', c0);
+          colorMix(unlerp(...radii, r), 'grow', c0, c1);
+          m.setColorAt(i, c0);
         },
         d);
 
@@ -505,6 +548,6 @@ olta.onUpdate(update);
 each(($b) => $b.addEventListener('click', () => {
     orbit.reset(true);
     // Doesn't seem to work with the above reset.
-    orbit.setLookAt(0, 0, orbitStart+orbit.minDistance, 0, 0, orbitStart, true);
+    // orbit.setLookAt(0, 0, orbitStart+orbit.minDistance, 0, 0, orbitStart, true);
   }),
   document.querySelectorAll('.recenter'));
